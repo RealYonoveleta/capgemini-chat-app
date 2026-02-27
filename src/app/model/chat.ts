3dd4a5739c6b5d92ca95bac7acc710974d4b3556
Jonas <martinezcuestajonas@gmail.com>
2026-02-27 09:51:58 +0100

    chore(models): removed id from models as firebase handles them appart

--- src/app/model/chat.ts
+++ src/app/model/chat.ts
@@ -1,7 +1,6 @@
 import { Timestamp } from 'firebase/firestore';
 
 export interface Chat {
-  id: string;
   title: string;
   participants: string[];
   lastMessage: string;
--- src/app/model/message.ts
+++ src/app/model/message.ts
@@ -1,7 +1,6 @@
 import { Timestamp } from 'firebase/firestore';
 
 export interface Message {
-  id: string;
   chatId: string;
   senderId: string;
   content: string;
